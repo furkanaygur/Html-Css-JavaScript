1192d11b81895e24b76311ad0e3731df540423dd
Furkan Aygur <32839776+furkanaygur@users.noreply.github.com>
2020-09-13 03:18:44 +0300

    Create main.js

---
+++ Flex/EdgeLedger-WebSite/js/main.js
@@ -0,0 +1,22 @@
+// Initialize and add the map
+function initMap() {
+  // Your location
+  const loc = { lat: 40.913981, lng: 29.299893 };
+  // Centered map on location
+  const map = new google.maps.Map(document.querySelector('.map'), {
+    zoom: 14,
+    center: loc
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({ position: loc, map: map });
+}
+
+// Sticky menu background
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
+
