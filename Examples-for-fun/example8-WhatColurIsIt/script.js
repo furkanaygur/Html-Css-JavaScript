a7cfcb377cd1846e024a630e41512c926cf261d1
Furkan Aygur <32839776+furkanaygur@users.noreply.github.com>
2021-02-10 22:40:46 +0300

    for fun

---
+++ Examples-for-fun/example8-WhatColurIsIt/script.js
@@ -0,0 +1,48 @@
+const body = document.querySelector("body")
+const clock = document.getElementById("clock")
+const color = document.getElementById("color")
+
+
+function showTime() {
+    let date = new Date()
+    let sec = date.getSeconds()
+    let min = date.getMinutes()
+    let hour = date.getHours()
+    let amPm = checkTime(hour)
+
+    changeColor(hour, min, sec)
+    clock.textContent = `${converTime(addZero(hour))}:${addZero(min)}:${addZero(sec)} ${amPm}`
+}
+
+function checkTime(hour) {
+    if (hour >= 12) {
+        return "PM"
+    }
+    else {
+        return "AM"
+    }
+}
+
+function converTime(hour) {
+    if (hour > 12) {
+        hour -= 12
+    }
+    return hour
+}
+
+function addZero(time) {
+    if (time < 10) {
+        time = "0" + time
+    }
+    return time
+}
+
+function changeColor(hour, min, sec) {
+    let colorValue = `#${hour}${min}${sec}`
+    color.textContent = colorValue
+    body.style.backgroundColor = colorValue
+}
+
+setInterval(() => {
+    showTime()
+}, 1000);
